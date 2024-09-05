6cd10ba243c30b092fd1deb19da584f7ff7a47e1
vatsalkr123 <rajsinghskrk6@gmail.com>
2024-09-05 14:07:33 +0530

    metadata changed

--- src/app/(root)/(home)/layout.tsx
+++ src/app/(root)/(home)/layout.tsx
@@ -4,7 +4,7 @@ import { Metadata } from 'next';
 import React from 'react'
 
 export const metadata: Metadata = {
-  title: 'YOOM',
+  title: 'MateMeet',
   description: 'A workspace for your team, powered by Stream Chat and Clerk.',
 };
 
